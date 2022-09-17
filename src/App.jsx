da13fd0ba378431a0ccbe52c3ea5b3e2ad5e06c9
LLCheeto <89275143+llcheeto@users.noreply.github.com>
2022-09-17 17:48:59 -0400

    created torus and made it render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from "react";
 import "./App.css";
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const scene = new THREE.Scene();
+  const gemoetry = new THREE.TorusGeometry(10, 3, 15, 100);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xff6347,
+    wireframe: true,
+  });
+  const torus = new THREE.Mesh(gemoetry, material);
 
-  return <div>Hello world </div>;
+  scene.add(torus);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+  });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.setZ(30);
+
+  return (
+    <div>
+      {renderer.render(scene, camera)} {animate()}
+    </div>
+  );
 }
 
 export default App;
